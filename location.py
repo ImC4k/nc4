140369c74647d6cae6ea82f677a01c6d8823cb23
ImC4k <calvinchu26@gmail.com>
2024-11-13 22:50:58 +0800

    land oriented location range

---
+++ location.py
@@ -0,0 +1,6 @@
+
+def get_coordinate_pairs(location):
+    # return [{"min_lat": 22.08, "max_lat": 22.35, "min_lon": 113.49, "max_lon": 114.31}]
+    return [
+        [(22.08, 113.49), (22.35, 114.31)]
+    ]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from nc4 import load_nc4, get_nc4_precipitation_stat, download_nc4, download_all_nc4_by_url_file
 from gis import load_gis, save_gis, augment_gis_lat_lon
+from location import get_coordinate_pairs
 import pandas as pd
 
 def download_all_nc4():
@@ -28,6 +29,7 @@ def process_location(location):
     output_gis_file_path = f'./GIS/precipitation-{location}.csv'
 
     gis = load_gis(input_gis_file_path, ['SID', 'SEASON', 'NUMBER', 'NAME', 'ISO_TIME', 'LAT', 'LON', 'LAT_MIN', 'LAT_MAX', 'LON_MIN', 'LON_MAX'], 'SID')
+    coordinate_pairs = get_coordinate_pairs(location)
     # download_all_nc4_for_gis(gis) # make subsequent calls faster
 
 
@@ -42,26 +44,39 @@ def process_location(location):
         day, month, year = iso_time.split('/') # variables are positional, do not remove unused ones
         if int(year) < 1998 or int(year) > 2023: # lacking nc4 data
             continue
-        # if row['SID'] != '2006268N12129': # disable this checking to run for all SIDs
-        #     continue
+        if row['SID'] != '2004231N09147': # disable this checking to run for all SIDs
+            continue
 
         data = load_nc4(row['ISO_TIME'])
-        precipitation_stat = get_nc4_precipitation_stat(data, row['LAT_MIN'], row['LAT_MAX'], row['LON_MIN'], row['LON_MAX'])
+        location_max = 0
+        location_data_count = 0
+        location_mean = 0
+        for coordinate_pair in coordinate_pairs:
+            min_coordinate, max_coordinate = coordinate_pair
+            min_lat, min_lon = min_coordinate
+            max_lat, max_lon = max_coordinate
+            precipitation_stat = get_nc4_precipitation_stat(data, min_lat, max_lat, min_lon, max_lon)
+            location_max = max(location_max, precipitation_stat["max"])
+            old_location_data_count = location_data_count
+            location_data_count += precipitation_stat["count"]
+            location_mean = (location_mean * old_location_data_count + precipitation_stat["mean"] * precipitation_stat["count"]) / location_data_count 
         # print(precipitation_stat)
-        row['PRECIPITATION_MAX'] = precipitation_stat["max"]
-        row['PRECIPITATION_MEAN'] = precipitation_stat["mean"]
-        # row['PRECIPITATION_MIN'] = precipitation_stat["min"]
-        row['LAT_MIN_INDEX'] = precipitation_stat["lat_min_index"]
-        row['LAT_MAX_INDEX'] = precipitation_stat["lat_max_index"]
-        row['LON_MIN_INDEX'] = precipitation_stat["lon_min_index"]
-        row['LON_MAX_INDEX'] = precipitation_stat["lon_max_index"]
+        row['PRECIPITATION_MAX'] = location_max
+        row['PRECIPITATION_MEAN'] = location_mean
+
+        del row["LAT_MIN"]
+        del row["LAT_MAX"]
+        del row["LON_MIN"]
+        del row["LON_MAX"]
+
 
         df = pd.concat([df, row.to_frame().T], ignore_index=True)
 
     save_gis(df, output_gis_file_path)
 
 def main():
-    locations = ['cambodia', 'guangdong', 'hongkong', 'laos', 'malaysia', 'myanmar', 'philippines', 'taiwan', 'thailand', 'vietnam']
+    # locations = ['cambodia', 'guangdong', 'hongkong', 'laos', 'malaysia', 'myanmar', 'philippines', 'taiwan', 'thailand', 'vietnam']
+    locations = ['hongkong']
     for location in locations:
         process_location(location)
 
